f5774f6542c43ae0cc85e04dd2073ff0bcdaa136
noatuh <noatuh@outlook.com>
2023-10-22 12:32:48 -0400

    added kelvin option

--- temperature-converter.js
+++ temperature-converter.js
@@ -1,13 +1,17 @@
 function convertTemperature() {
-    const temperature = document.getElementById("temperature").value;
-    const scale = document.getElementById("scale").value;
-  
-    if (scale === "celsius") {
-      const convertedTemperature = (temperature * 9/5) + 32;
-      document.getElementById("result").textContent = `${temperature} Celsius is ${convertedTemperature} Fahrenheit`;
-    } else {
-      const convertedTemperature = (temperature - 32) * 5/9;
-      document.getElementById("result").textContent = `${temperature} Fahrenheit is ${convertedTemperature} Celsius`;
-    }
+  const temperature = parseFloat(document.getElementById("temperature").value);
+  const scale = document.getElementById("scale").value;
+  let convertedTemperature;
+
+  if (scale === "celsius") {
+      convertedTemperature = (temperature * 9/5) + 32;
+      document.getElementById("result").textContent = `${temperature}° Celsius is ${convertedTemperature.toFixed(2)}° Fahrenheit`;
+  } else if (scale === "fahrenheit") {
+      convertedTemperature = (temperature - 32) * 5/9;
+      document.getElementById("result").textContent = `${temperature}° Fahrenheit is ${convertedTemperature.toFixed(2)}° Celsius`;
+  } else if (scale === "kelvin") {
+      let celsiusEquivalent = temperature - 273.15;
+      let fahrenheitEquivalent = temperature * 9/5 - 459.67;
+      document.getElementById("result").textContent = `${temperature}° Kelvin is ${celsiusEquivalent.toFixed(2)}° Celsius and ${fahrenheitEquivalent.toFixed(2)}° Fahrenheit`;
   }
-  
+}
